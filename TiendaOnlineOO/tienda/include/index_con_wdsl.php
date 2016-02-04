c5918f16c852909c5e245c53a92f90cfcd2aec77
jjaviergr1176 <jjaviergr1176@gmail.com>
2016-02-04 22:40:54 +0100

    REproductor con lista funciona en bucle.

--- TiendaOnlineOO/tienda/include/index_con_wdsl.php
+++ TiendaOnlineOO/tienda/include/index_con_wdsl.php
@@ -10,20 +10,19 @@
         require_once('DB.php');
 
        $uri = "http://localhost/tienda_wdsl/TiendaOnlineOO/tienda/include";
-      // $url = "$uri/servicio.php";
+    
 
         // $cliente = new SoapClient(null, array('location' => $url, 'uri' => $uri));
        $cliente = new SoapClient("$uri/BD_Proxy.wsdl");
 
-        //$producto = $cliente->obtieneProducto("3DSNG"); //lo hace bien
-        //$otro_precio=$producto->getPVP(); //no funciona por no soportar el serv esta clase.
+       
 
 
         $precio = $cliente->obtienePrecioProducto("3DSNG"); //correcto
         echo ("precio " + $precio); // funciona correctamente.
 
 
-        //echo ("precio de producto " + $otro_precio);
+     
         ?>
     </body>
 </html>
